8d91d43b325fa298c503ccf6fb1a778346bd8594
Finn McCoy <finnmccoy99@gmail.com>
2022-11-25 10:16:32 +0000

    The cohort of students can now be input by the user

--- directory.rb
+++ directory.rb
@@ -3,10 +3,16 @@ def input_students
   puts "To finish, just hit return twice"
   students =  []
   name = gets.chomp
-  while !name.empty?
-    students << {name: name, cohort: :november}
+  puts "And which cohort is this student in?"
+  cohort = gets.chomp
+  while !(name.empty? && cohort.empty?)
+    name = "Unknown name" if name.empty?
+    cohort = :november if cohort.empty?
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students."
     name = gets.chomp
+    puts "And which cohort is this student in?"
+    cohort = gets.chomp
   end
   students
 end
@@ -17,13 +23,17 @@ def print_header
 end
 
 def print(students)
-  students.each { |student| 
-  puts "#{student[:name]} (#{student[:cohort]} cohort)" 
+  students.each.with_index(1) { |student, index| 
+  puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)".center(20)
   }
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+  if names.count > 0
+    puts "Overall, we have #{names.count} great students."
+  else
+    puts "We have no students!"
+  end
 end
 
 students = input_students
