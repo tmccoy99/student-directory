12a18a9dc5f28d30b055f007e61f4ef3b6dce73f
Finn McCoy <finnmccoy99@gmail.com>
2022-11-25 15:03:22 +0000

    Refactored interactive menu into several methods

--- directory.rb
+++ directory.rb
@@ -1,20 +1,20 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students =  []
   name = gets.chomp
   puts "And which cohort is this student in?"
   cohort = gets.chomp
   while !(name.empty? && cohort.empty?)
     name = "Unknown name" if name.empty?
     cohort = :november if cohort.empty?
-    students << {name: name, cohort: cohort.to_sym}
-    puts "Now we have #{students.count} students."
+    @students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{@students.count} students."
     name = gets.chomp
     puts "And which cohort is this student in?"
     cohort = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -22,30 +22,59 @@ def print_header
   puts "-------------"
 end
 
-def print_by_cohort(students)
+def print_students_list
   students_by_cohort = Hash.new {|hash, key| hash[key] = []}
-  students.each { |student|
-  name = student[:name] ; cohort = student[:cohort]
-  students_by_cohort[cohort] << name }
-  students_by_cohort.each { |cohort, students|
+  @students.each do |student|
+    name = student[:name] ; cohort = student[:cohort]
+    students_by_cohort[cohort] << name
+  end
+  students_by_cohort.each do |cohort, some_students|
     puts "In the #{cohort} cohort we have"
-    students.each { |student| puts student }
-    }
+    some_students.each { |student| puts student }
+  end
 end
 
-def print_footer(names)
-  num_names = names.count
-  case num_names
+def print_footer
+  case @students.count
     when 0
       puts "We have no students!"
     when 1
       puts "Overall, we have 1 great student"
     else
-      puts "Overall, we have #{num_names} great students"
+      puts "Overall, we have #{@students.count} great students"
   end
 end
 
-students = input_students
-print_header
-print_by_cohort(students)
-print_footer(students)
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Invalid input"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
