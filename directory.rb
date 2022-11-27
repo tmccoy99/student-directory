59f43ed02bcaca285ada989aaf1c4f26e43f04cc
Finn McCoy <finnmccoy99@gmail.com>
2022-11-27 14:38:07 +0000

    The students.csv file will be loaded by default if no arguments provided

--- directory.rb
+++ directory.rb
@@ -2,7 +2,7 @@
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
+  filename = "students.csv" if filename.nil?
   if File.exist?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} students from #{filename}"
